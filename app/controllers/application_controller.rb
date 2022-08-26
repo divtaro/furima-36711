b13e7b293db1c178aae70fb55777248b4217e2c4
divtaro <katodai.pg@gmail.com>
2022-08-26 13:11:26 +0900

    新規登録機能（保存）の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-  
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -9,4 +9,9 @@ class ApplicationController < ActionController::Base
       username == 'admin' && password == '1111'
     end
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :encrypted_password, :last_name, :first_name, :last_name_kana, :first_name_kana, :date_birth])
+  end
+
 end

6a5beed3b24ab87696e175f3aeb8b51cd0d74229
divtaro <katodai.pg@gmail.com>
2022-08-26 18:13:50 +0900

    Userモデルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "全角（漢字・ひらがな・カタカナ）で入力してください"}
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "全角（漢字・ひらがな・カタカナ）で入力してください"}
+  validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: "全角（カタカナ）で入力してください"}
+  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: "全角（カタカナ）で入力してください"}
 
+  # VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  # validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります' }
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "Include both letters and numbers"}
+  validates :date_birth, presence: true
 end
+  

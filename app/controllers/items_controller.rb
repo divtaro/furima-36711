a5b471676103d1c0e83184a29a0695104a0abdd2
divtaro <katodai.pg@gmail.com>
2022-08-30 18:38:15 +0900

    itemsコントローラーにストロングパラメーター設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,10 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :description, :category_id, :status_id, :delivery_charge_id, :prefecture_id, :days_ship_id, :price, :image).merge(user_id: current_user.id)
+  end
 end

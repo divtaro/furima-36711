b80f065611f0d28e7d04ead2bf82c8cc5db55b64
divtaro <katodai.pg@gmail.com>
2022-08-30 17:55:33 +0900

    delivery_chargeモデル記述（データ定義、バリデーション、アソシエーション）

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,10 +10,12 @@ class Item < ApplicationRecord
   validates :user,               presence: true
 
   #ジャンルの選択が「---」の時は保存できないようにする
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank" }
-  validates :status_id,   numericality: { other_than: 1 , message: "can't be blank" }
+  validates :category_id,        numericality: { other_than: 1 , message: "can't be blank" }
+  validates :status_id,          numericality: { other_than: 1 , message: "can't be blank" }
+  validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank" }
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
+  belongs_to :delivery_charge
 end

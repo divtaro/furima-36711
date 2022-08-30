734b830217ef1bb3208576542f8f3247fde97389
divtaro <katodai.pg@gmail.com>
2022-08-30 18:11:25 +0900

    days_shipモデル記述（データ定義、バリデーション、アソシエーション）

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,10 +14,12 @@ class Item < ApplicationRecord
   validates :status_id,          numericality: { other_than: 1 , message: "can't be blank" }
   validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank" }
   validates :prefecture_id,      numericality: { other_than: 1 , message: "can't be blank" }
+  validates :days_ship_id,       numericality: { other_than: 1 , message: "can't be blank" }
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
   belongs_to :delivery_charge
   belongs_to :prefecture
+  belongs_to :days_ship
 end

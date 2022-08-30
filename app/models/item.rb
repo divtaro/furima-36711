b98e6f0e26979af336b17f27796db3a8eff7eb05
divtaro <katodai.pg@gmail.com>
2022-08-30 17:47:53 +0900

    statusモデル記述（データ定義、バリデーション、アソシエーション）

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,8 +10,10 @@ class Item < ApplicationRecord
   validates :user,               presence: true
 
   #ジャンルの選択が「---」の時は保存できないようにする
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :status_id,   numericality: { other_than: 1 , message: "can't be blank" }
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :status
 end

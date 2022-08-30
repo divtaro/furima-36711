c76c92b41c0ca6963d48b3da32d71d2637890593
divtaro <katodai.pg@gmail.com>
2022-08-30 17:35:27 +0900

    categoryモデル記述（データ定義、バリデーション、アソシエーション）

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,17 @@
 class Item < ApplicationRecord
+  validates :item_name,          presence: true
+  validates :description,        presence: true
+  validates :category_id,        presence: true
+  validates :status_id,          presence: true
+  validates :delivery_charge_id, presence: true
+  validates :prefecture_id,      presence: true
+  validates :days_ship_id,       presence: true
+  validates :price,              presence: true
+  validates :user,               presence: true
+
+  #ジャンルの選択が「---」の時は保存できないようにする
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
 end

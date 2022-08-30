67624a18e0508a685ec1537aa8fb0c0790ad78d3
divtaro <katodai.pg@gmail.com>
2022-08-30 16:26:50 +0900

    マイグレーションファイルへのカラム追加、itemsテーブル作成

--- db/migrate/20220830071001_create_items.rb
+++ db/migrate/20220830071001_create_items.rb
@@ -1,7 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.string     :item_name,          null: false
+      t.string     :description,        null: false
+      t.string     :category_id,        null: false
+      t.string     :status_id,          null: false
+      t.string     :delivery_charge_id, null: false
+      t.string     :prefecture_id,      null: false
+      t.string     :days_ship_id,       null: false
+      t.string     :price,              null: false
+      t.references :user,        foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_25_144117) do
+ActiveRecord::Schema.define(version: 2022_08_30_071001) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "item_name", null: false
+    t.string "description", null: false
+    t.string "category_id", null: false
+    t.string "status_id", null: false
+    t.string "delivery_charge_id", null: false
+    t.string "prefecture_id", null: false
+    t.string "days_ship_id", null: false
+    t.string "price", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -30,4 +45,5 @@ ActiveRecord::Schema.define(version: 2022_08_25_144117) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end

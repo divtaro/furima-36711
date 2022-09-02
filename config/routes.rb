b625802bad5fa05b0d2160e7a151618508a6fc37
divtaro <katodai.pg@gmail.com>
2022-09-02 20:43:03 +0900

    editアクションのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   # get 'items/index ←コントローラー作成時に自動で作成された
   root to: "items#index"
-  resources :items, only: [:index, :new, :create, :show]
+  resources :items, only: [:index, :new, :create, :show, :edit]
 end

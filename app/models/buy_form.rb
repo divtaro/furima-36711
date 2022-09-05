49a68bd69c870f16de9716a415e231e40c0c057a
divtaro <katodai.pg@gmail.com>
2022-09-05 17:02:34 +0900

    購入情報のバリデーション削除（購入情報は購入が完了した結果なのでバリデーションは不要）

--- app/models/buy_form.rb
+++ app/models/buy_form.rb
@@ -10,12 +10,13 @@ class BuyForm
     validates :municipalities
     validates :house_num
     validates :tel
-    validates :buy_record_id
+    # //validates :buy_record_idは購入が完了した結果、作られるデータであるため、購入画面の入力内容をジャッジするモデル（formオブジェクト）では不要。外さなければ不要なエラーメッセージが表示される
   end
   validates :prefecture_id,  numericality: { other_than: 1, message: "can't be blank" }
 
   def save
     buy_record = BuyRecord.create(user_id: user_id, item_id: item_id)
-    ShipAddress.create(post_code: post_code, prefecture_id: prefecture_id, municipalities: municipalities, house_num: house_num, building: building, tel: tel, buy_record_id: buy_record.id)
+    ShipAddress.create(post_code: post_code, prefecture_id: prefecture_id, municipalities: municipalities,
+                       house_num: house_num, building: building, tel: tel, buy_record_id: buy_record.id)
   end 
 end

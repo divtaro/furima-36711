dd0be55dcc4f268ea5a1bbcbfe7829c1d73f0247
divtaro <katodai.pg@gmail.com>
2022-09-06 16:20:59 +0900

    formオブジェクトのFactoryBot作成

---
+++ spec/factories/buy_forms.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :buy_form do
+    user_id         { '1' }
+    item_id         { '1' }
+    post_code       { '123-4567' }
+    prefecture_id   { '2' }  
+    municipalities  { '名古屋市' }
+    house_num       { '1-1-1' }
+    building        { '' }
+    tel             { '09012345678' }
+  end
+end

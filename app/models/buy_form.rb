dfbba3caddff14ab20e728cd0ca9eaea78c6bbdd
divtaro <katodai.pg@gmail.com>
2022-09-06 16:22:22 +0900

    formオブジェクトのバリデーション修正

--- app/models/buy_form.rb
+++ app/models/buy_form.rb
@@ -6,10 +6,10 @@ class BuyForm
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :post_code,      format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :post_code,      format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "は、半角数字3桁＋ハイフン（-）＋半角数字4桁の形式で入力してください。"}
     validates :municipalities
     validates :house_num
-    validates :tel
+    validates :tel,            format: {with: /\A\d{10,11}\z/, message: "は10桁以上11桁以内の半角数値のみで入力してください"}
     # //validates :buy_record_idは購入が完了した結果、作られるデータであるため、購入画面の入力内容をジャッジするモデル（formオブジェクト）では不要。外さなければ不要なエラーメッセージが表示される
   end
   validates :prefecture_id,  numericality: { other_than: 1, message: "can't be blank" }

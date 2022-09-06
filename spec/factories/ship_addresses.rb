9f0a5e7c76904e86b04e9c7a75096885cbda1fc2
divtaro <katodai.pg@gmail.com>
2022-09-06 16:23:43 +0900

    不要なモデル、テストコード保留

--- spec/factories/ship_addresses.rb
+++ spec/factories/ship_addresses.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :ship_address do
-    
+    # post_code       { '123-4567' }
+    # prefecture_id   { '2' }  
+    # municipalities  { '名古屋市' }
+    # house_num       { '1-1-1' }
+    # building        { '' }
+    # tel             { '09012345678' }
   end
 end
